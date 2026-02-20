ac899a070854612997d0c6799aa4c695e64f7bde
Gurpreet Kait <gurpreetkait.codes@gmail.com>
2026-02-21 01:55:57 +0530

    fix

--- database/migrations/2024_01_01_000005_create_posts_table.php
+++ database/migrations/2024_01_01_000005_create_posts_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -24,7 +25,9 @@ return new class extends Migration
             $table->timestamp('published_at')->nullable();
             $table->timestamps();
 
-            $table->fullText(['title', 'body']);
+            if (DB::getDriverName() !== 'sqlite') {
+                $table->fullText(['title', 'body']);
+            }
             $table->index('vote_count');
             $table->index('published_at');
         });
